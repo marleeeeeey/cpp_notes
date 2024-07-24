fc774f916975fb5f0d37f982ed704c77ccf602e0
marleeeeeey <marleeeeeey@gmail.com>
2024-07-24 13:12:12 +0400

    Add note.

--- code/fedr_pazzle_002_deduce_this.cpp
+++ code/fedr_pazzle_002_deduce_this.cpp
@@ -17,10 +17,10 @@ int main()
 // > void f(this A); A{}.f(); по сути эквивалентно static void f(A); f(A{});
 // эквивалентно, но не совсем )
 // https://stackoverflow.com/q/78785176/7325599
-
 // ST
 // static void f(A&&); f(A{}); - а не такой эквивалент должен быть? У нас же там rvalue? Deducing THIS умеет выводить
 // rvalue ref?
-
 // EB
 // чтоб было так, как ты говоришь, сигнатура у метода должна была бы быть такой void f(this A &&);
+// SUMMARY:
+// Как я понял, operator.() вызывает материализацю обекта, т.е. превращение prvalue -> glvalue.
